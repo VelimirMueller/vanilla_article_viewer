9f4d620cf07d7fe06978c0a3428e06f8db42cf91
velimir <velimir.mueller@googlemail.com>
2024-12-11 18:08:37 +0100

    add logic to handle overlay display by button click - the handleOverlay function returns a closure and holds and handles the state of the visibility of the overlay

---
+++ js/utility/handleOverlay.js
@@ -0,0 +1,32 @@
+
+/**
+ * Toggles the state of the overlay and hold the state of the overlay visibility.
+ * 
+ * ---
+ * Example usage:
+ * - initialize handler:
+ * `const overlayHandler = handleOverlay(false)`
+ * 
+ * - toggle overlay:
+ * `overlayHandler.toggleOverlay('template__overlay')`
+ * @param { string } state
+ */
+export const handleOverlay = (state) => {
+    let isOverlayVisible = state
+
+    return {
+        /**
+         * Toggles the overlay
+         * @param {string} overlayId
+         */
+        toggleOverlay: (overlayId) => {
+            if (!isOverlayVisible) {
+                document.getElementById(overlayId).style.display = 'none';
+            } else {
+                document.getElementById(overlayId).style.display = 'block';
+            }
+
+            isOverlayVisible = !isOverlayVisible
+        }
+    }   
+}
