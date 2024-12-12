c8e389673c09e6d7caaa222b7fc6acf7eb33579e
velimir <velimir.mueller@googlemail.com>
2024-12-12 23:12:28 +0100

    Refactoring: - separation of concerns with functions -improve doc block -add explanations as comments -reduce cognitive load

--- js/utility/handleOverlay.js
+++ js/utility/handleOverlay.js
@@ -1,6 +1,8 @@
 
 /**
  * Toggles the state of the overlay and hold the visibility state in `isOverlayVisible`.
+ * Also handles `aria-pressed` and `aria-hidden` attributes for the overlay element
+ * and for both overlay toggle buttons.
  * 
  * ---
  * Example usage:
@@ -29,15 +31,7 @@ export const handleOverlay = (state) => {
             }
             
             // Toggle visibility and set aria-hidden attribute accordingly on overlay visibility
-            if (isOverlayVisible && overlayElement && overlayElement !== null) {
-                overlayElement.setAttribute('aria-hidden', 'true')
-                toggleButton.setAttribute('aria-expanded', 'false')
-                overlayElement.style.display = 'none'
-            } else if (document.getElementById(overlayId) !== null) {
-                overlayElement.setAttribute('aria-hidden', 'false')
-                toggleButton.setAttribute('aria-expanded', 'true')
-                overlayElement.style.display = 'flex'
-            }
+            toggleVisibility(overlayId, isOverlayVisible, overlayElement, toggleButton)
             
             // Toggle the state within the outer function's scope
             isOverlayVisible = !isOverlayVisible
@@ -48,4 +42,48 @@ export const handleOverlay = (state) => {
             return isOverlayVisible
         }
     }   
+}
+
+/**
+ * Helper function to handle overlay visibility
+ * ___
+ * @param {string} overlayId 
+ * @param {boolean} isOverlayVisible 
+ * @param {HTMLElement} overlayElement 
+ * @param {HTMLElement} toggleButton 
+ */
+const toggleVisibility = (overlayId, isOverlayVisible, overlayElement, toggleButton) => {
+  if (isOverlayVisible && overlayElement && overlayElement !== null) {
+    setVisible(overlayElement, toggleButton)
+  } else if (document.getElementById(overlayId) !== null) {
+    setInvisible(overlayElement, toggleButton)
+  }
+}
+
+/**
+ * Helper function to set visibility from `display:flex` to 
+ * `display:none` along with aria-hidden and
+ * aria-pressed attributes for the overlay and toggle buttons.
+ * ___
+ * @param {HTMLElement} overlay 
+ * @param {HTMLElement} button 
+ */
+const setVisible = (overlay, button) => {
+  overlay.setAttribute('aria-hidden', 'true')
+  button.setAttribute('aria-pressed', 'false')
+  overlay.style.display = 'none'
+}
+
+/**
+ * Helper function to set visibility from `display:none` to 
+ * `display:flex` along with aria-hidden and
+ * aria-pressed attributes for the overlay and toggle buttons.
+ * ___
+ * @param {HTMLElement} overlay 
+ * @param {HTMLElement} button 
+ */
+const setInvisible = (overlay, button) => {
+  overlay.setAttribute('aria-hidden', 'false')
+  button.setAttribute('aria-pressed', 'true')
+  overlay.style.display = 'flex'
 }
