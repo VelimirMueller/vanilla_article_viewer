3fb530f945489597e062d8309280ea1f9b0acbd8
velimir <velimir.mueller@googlemail.com>
2024-12-12 23:21:11 +0100

    Refactor: fetchArticles needed improvements in error handling and imrpove DX by using async await insted of then

--- js/utility/fetchArticles.js
+++ js/utility/fetchArticles.js
@@ -5,7 +5,15 @@
  */
 export const fetchArticles = async () => {
     try {
-        return fetch('/data/articles.json').then(response => response.json())
+        const response = await fetch('/data/articles.json')
+
+        if (!response.ok) {
+            throw new Error(`Oooops! Something went wrong loading the local file. status: ${response.status} text: ${response.statusText}`)
+        }
+
+        const data = response.json()
+
+        return data
     } catch (err) {
         throw new Error(`Error while loading data. error: ${err}`)
     }
