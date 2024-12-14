30969d2f8d37e3398d90b61666dcbe22a2ff2a42
velimir <velimir.mueller@googlemail.com>
2024-12-14 12:04:46 +0100

    reate article buttons now with a factory so the functions will do just one thing

--- js/articles/createButtons.js
+++ js/articles/createButtons.js
@@ -12,27 +12,48 @@
  * @param {string} paragraphTargetId 
  */
 export const createArticleButtons = (buttonContainerId, articles, btnClassList) => {
-    try{
+    try {
       const buttonContainer = document.getElementById(buttonContainerId)
       // initialize document fragment which will be added to the DOM later
       const fragments = document.createDocumentFragment()
 
       // create article buttons and add attributes
-      articles.forEach(article => {
-        const button = document.createElement("button")
-        
-        button.id = `${article.id}`
-        button.textContent = article.headline
-        button.type = 'button'
-        button.classList = btnClassList
-        
-        // add button to fragments
-        fragments.appendChild(button)
-      })
+      const buttonFragments = articleButtonFactory(articles, btnClassList, fragments)
       
       // apply DOM changes to button container
-      buttonContainer.appendChild(fragments)
+      buttonContainer.appendChild(buttonFragments)
     } catch (err) {
       throw new Error(`Failed to create buttons. error: ${err}`)
     }
+}
+
+/**
+ * #### Simple button factory that set certain attributes right away.
+ * ____
+ * @param { array } items 
+ * @param { string } btnClassList 
+ * @param { DocumentFragment } fragments 
+ * @returns { DocumentFragment }
+ */
+const articleButtonFactory = (items, btnClassList, fragment) => {
+  const result = fragment
+
+  if (Array.isArray(items) && items.length >= 1 && items !== null) {
+    items.forEach(item => {
+      const button = document.createElement('button')
+      const { id, headline } = item
+
+      button.id = id
+      button.type = 'button'
+      button.textContent = headline
+      button.classList = btnClassList
+
+      button.setAttribute('aria-describedby', 'article-paragraph')
+      button.setAttribute('role', 'menuitem')
+
+      result.appendChild(button)
+    })
+  }
+
+  return result
 }
